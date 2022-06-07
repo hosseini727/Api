96c076e68f8b337d9c4f47e5ec3f68db2f8a7afc
esmaeil7 <esmaeil.hosseini727@gmail.com>
2022-06-07 08:31:23 +0430

    eeeee

--- Api/WeatherForecast.cs
+++ Api/WeatherForecast.cs
@@ -4,12 +4,12 @@ namespace Api
 {
     public class WeatherForecast
     {
+        //test
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
 
         public string Summary { get; set; }
 
